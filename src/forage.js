28478d568d533729f68f0eeaa54e116ba26a69ac
Ryhan Hassan <rthprog@gmail.com>
2012-10-10 09:37:17 -0400

    Autosuggest demo created

--- src/forage.js
+++ src/forage.js
@@ -5,13 +5,14 @@
  * ryhanh@me.com
  */
 
-
-
-function analyze( query , callback)
+function forage( query , callback)
 {
+	console.log(query);
 	// Fetch tweets based on the provided query,
 	// and then pass the data to handleTweets();
-	return fetchTweets( query, handleTweets, true);
+	var init = function(){
+		fetchTweets( query, handleTweets, true);
+	}
 
 	// Given tweet data, santize the data,
 	// analyze the content, and then return the entities
@@ -27,99 +28,113 @@ function analyze( query , callback)
 		return cleanString(string);
 	}
 
+	// Pull out category data
+	var processCategories = function(contentData){
+		var catGroup = contentData.query.results.yctCategories;
+		if (catGroup != undefined && catGroup.yctCategory != undefined)
+		{
+			var topicData = catGroup.yctCategory;
+			return _.without(_.compact(_.pluck(topicData, 'content')), query);
+		}
+		return [];
+	}
+
+	// Pull out related entity data
+	var processRelated = function(contentData){
+		var entGroup = contentData.query.results.entities;
+		if (entGroup != undefined && entGroup.entity != undefined)
+		{
+			var entityData = entGroup.entity;
+
+			if (entityData.length != undefined){
+				entityData = _.pluck(entityData, 'text');
+			}
+			else{
+				entityData = [entityData.text];
+			}
+
+			return _.without(_.compact(_.uniq(_.pluck(entityData,'content'))), query);
+		}
+	}
+
 	// Given the content analyzed data, return 
 	// a JSON object containing the categories
 	// that this query belongs to, as well 
 	// as related entites
 	var getEntites = function(contentData)
 	{
-		if (contentData.query.results != undefined)
+		/*
+		callback({
+				categories: processCategories(contentData),
+				related: processRelated(contentData)
+			});
+		*/
+		if (contentData.query!= undefined && contentData.query.results != undefined)
 		{
-			var categories,
-				related;
-
-			// Pull out category data
-			var catGroup = contentData.query.results.yctCategories;
-			if (catGroup != undefined && catGroup.yctCategory != undefined)
-			{
-				var topicData = catGroup.yctCategory;
-				categories = _.compact(_.pluck(topicData, 'content'));
-			}
-
-			// Pull out related entity data
-			var entGroup = contentData.query.results.entities;
-			if (entGroup != undefined && entGroup.entity != undefined)
-			{
-				var entityData = entGroup.entity;
-
-				if (entityData.length != undefined){
-					entityData = _.pluck(entityData, 'text');
-				}
-				else{
-					entityData = [entityData.text];
-				}
-
-				related = _.uniq(_.pluck(entityData,'content'));
-			}
-
-			// Call the callback function, returning the expected data
 			callback({
-				categories: categories,
-				related: related
+				categories: processCategories(contentData),
+				related: processRelated(contentData)
 			});
 		}
 		else
 		{
-			// Some error occurred, so return {empty}
 			callback({});
 		}
 	}
-}
 
+	/*
+	 * Helper Functions
+	 */
 
-function fetchTweets( query, callback, noise)
-{
-	$.ajax({
-		url : 'http://search.twitter.com/search.json',
-		jsonp: 'callback',
-		dataType : 'jsonp',
-		data : {
-			q : query,
-			lang : 'en',
-			result_type : (noise == true) ? 'mixed' : 'popular',
-			rpp : 50,
-			show_user : true
-		},
-		success : function (data){ return callback(data);}
-	});
-}
+	// Return data related to tweets that match query
+	function fetchTweets( query, callback, noise)
+	{
+		$.ajax({
+			url : 'http://search.twitter.com/search.json',
+			jsonp: 'callback',
+			dataType : 'jsonp',
+			data : {
+				q : query,
+				lang : 'en',
+				result_type : (noise == true) ? 'mixed' : 'popular',
+				rpp : 50,
+				show_user : true
+			},
+			success : function (data){ callback(data);}
+		});
+	}
 
-function contentAnalyze( string, callback )
-{
-	$.ajax({
-	  url : 'http://query.yahooapis.com/v1/public/yql',
-	  jsonp : 'callback',
-	  dataType : 'jsonp',
-	  data : {
-	    q : "select * from contentanalysis.analyze where text=\"" + string + "\"",
-		format : 'json'
-	  },
-	  success : function(data){ return callback(data);}
-	});
+	// Analyze the content of a string
+	function contentAnalyze( string, callback )
+	{
+		$.ajax({
+		  url : 'http://query.yahooapis.com/v1/public/yql',
+		  jsonp : 'callback',
+		  dataType : 'jsonp',
+		  data : {
+		    q : "select * from contentanalysis.analyze where text=\"" + string + "\"",
+			format : 'json'
+		  },
+		  success : function(data){ return callback(data);}
+		});
 
-}
+	}
 
-var cleanString = function(string){
-	return string
-		.replace(/"/g,'')
-	    .replace(/[\n\r]/g, '')
-	    .replace(/“/g,'')
-	    .replace(/'/g,'')
-	    .replace(/\//g,'')
-	    .replace(/&/g,'')
-	    .replace(/%/g,'')
-	    .replace(/\./g,'')
-	    .replace(/\=/g,'');
+	// Remove problematic characters from a string
+	var cleanString = function(string){
+		return string
+			.replace(/"/g,'')
+		    .replace(/[\n\r]/g, '')
+		    .replace(/“/g,'')
+		    .replace(/'/g,'')
+		    .replace(/\//g,'')
+		    .replace(/&/g,'')
+		    .replace(/%/g,'')
+		    .replace(/\./g,'')
+		    .replace(/\=/g,'');
+	}
+
+	init();
 }
 
 function self(data){
